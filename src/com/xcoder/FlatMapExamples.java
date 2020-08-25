b20552f0d6fccd8250ccc611bb2daa58da89d581
ginamdar <guru.inamdar@gmail.com>
2020-08-24 23:04:12 -0400

    module-6 custom collectors

--- src/com/xcoder/FlatMapExamples.java
+++ src/com/xcoder/FlatMapExamples.java
@@ -16,10 +16,10 @@ public class FlatMapExamples {
     public static void main(String[] args) throws IOException {
         // http://introcs.cs.princeton.edu/java/data/TomSawyer.txt
         
-        Stream<String> stream1 = Files.lines(Paths.get("files/TomSawyer_01.txt")) ;
-        Stream<String> stream2 = Files.lines(Paths.get("files/TomSawyer_02.txt")) ;
-        Stream<String> stream3 = Files.lines(Paths.get("files/TomSawyer_03.txt")) ;
-        Stream<String> stream4 = Files.lines(Paths.get("files/TomSawyer_04.txt")) ;
+        Stream<String> stream1 = Files.lines(Paths.get("maps-and-flatmap/files/TomSawyer_01.txt")) ;
+        Stream<String> stream2 = Files.lines(Paths.get("maps-and-flatmap/files/TomSawyer_02.txt")) ;
+        Stream<String> stream3 = Files.lines(Paths.get("maps-and-flatmap/files/TomSawyer_03.txt")) ;
+        Stream<String> stream4 = Files.lines(Paths.get("maps-and-flatmap/files/TomSawyer_04.txt")) ;
 
 //        System.out.println("Stream 1 : " + stream1.count());
 //        System.out.println("Stream 2 : " + stream2.count());
